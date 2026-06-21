6253d49d2aead2ed8a98cac3fbc6a09f63f796e5
Shreyansh Golchha <shreyansh.golchha2023@ssipmt.com>
2026-06-21 19:53:49 +0530

    Time: 12 ms (80.62%), Space: 87.7 MB (9.29%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        sort(costs.begin(),costs.end());
-        int count = 0;
-        if(coins<costs[0]) return 0;
-        int i = 0;
-        while(coins!=0 && i<costs.size())
+        int k = costs[0];
+        for(int i = 0;i<costs.size();i++)
         {
-            if(coins<costs[i]) return count;
-            coins-=costs[i];
-            count++;
+            k = max(k,costs[i]);
+        }
+        vector<int> count(k+1, 0);
+        for(int i = 0;i<costs.size();i++)
+        {
+            count[costs[i]]++;
+        }
+        for(int i = 1;i<k+1;i++)
+        {
+            count[i] = count[i-1]+count[i];
+        }
+        vector<int> sorted(costs.size(),0);
+        for(int i = costs.size()-1;i>=0;i--)
+        {
+            int a = count[costs[i]];
+            sorted[a-1] = costs[i];
+            count[costs[i]]--;
+        }
+        int counted = 0;
+        if(coins<sorted[0]) return 0;
+        int i = 0;
+        while(coins!=0 && i<sorted.size())
+        {
+            if(coins<sorted[i]) return counted;
+            coins-=sorted[i];
+            counted++;
             i++;
         }
-        return count;
+        return counted;
     }
 };
