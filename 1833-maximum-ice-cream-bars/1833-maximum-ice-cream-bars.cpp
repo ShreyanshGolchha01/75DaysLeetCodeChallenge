0e029ea4a7009c9f4bb1a00d4844646aa9f4e577
Shreyansh Golchha <shreyansh.golchha2023@ssipmt.com>
2026-06-21 19:55:30 +0530

    Time: 8 ms (86.81%), Space: 87.6 MB (9.29%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -2,36 +2,27 @@ class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
         int k = costs[0];
-        for(int i = 0;i<costs.size();i++)
+        for(int x : costs)
+            k = max(k, x);
+        vector<int> count(k + 1, 0);
+        for(int x : costs)
+            count[x]++;
+        for(int i = 1; i <= k; i++)
+            count[i] += count[i - 1];
+        vector<int> sorted(costs.size());
+        for(int i = costs.size()-1; i>=0; i--)
         {
-            k = max(k,costs[i]);
-        }
-        vector<int> count(k+1, 0);
-        for(int i = 0;i<costs.size();i++)
-        {
-            count[costs[i]]++;
-        }
-        for(int i = 1;i<k+1;i++)
-        {
-            count[i] = count[i-1]+count[i];
-        }
-        vector<int> sorted(costs.size(),0);
-        for(int i = costs.size()-1;i>=0;i--)
-        {
-            int a = count[costs[i]];
-            sorted[a-1] = costs[i];
+            sorted[count[costs[i]] - 1] = costs[i];
             count[costs[i]]--;
         }
-        int counted = 0;
-        if(coins<sorted[0]) return 0;
-        int i = 0;
-        while(coins!=0 && i<sorted.size())
+        int ans = 0;
+        for(int x : sorted)
         {
-            if(coins<sorted[i]) return counted;
-            coins-=sorted[i];
-            counted++;
-            i++;
+            if(coins < x)
+                break;
+            coins -= x;
+            ans++;
         }
-        return counted;
+        return ans;
     }
 };
