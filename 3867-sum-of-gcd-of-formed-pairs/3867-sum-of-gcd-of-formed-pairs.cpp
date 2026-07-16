197526ca6a571a2a62d856997530cf39b417f2c6
Shreyansh Golchha <shreyansh.golchha2023@ssipmt.com>
2026-07-16 21:18:58 +0530

    Time: 56 ms (97.54%), Space: 155.6 MB (75.58%) - LeetHub

--- 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
@@ -1,13 +1,5 @@
 class Solution {
 public:
-    int gcd(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
-    }
     long long gcdSum(vector<int>& nums) {
         if(nums.size()==1) return 0;
         vector<int> pref(nums.size());
