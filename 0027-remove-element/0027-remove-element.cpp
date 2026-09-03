fbed7da5feaa6242ff2e55a6a53751612d566629
Shreyansh Golchha <shreyansh.golchha2023@ssipmt.com>
2026-09-04 00:19:31 +0530

    Time: 0 ms (100%), Space: 11.8 MB (12.5%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -3,19 +3,20 @@ public:
     int removeElement(vector<int>& nums, int val) {
         int i = 0;
         int j = nums.size() - 1;
-        if (nums.size()<1) return 0;
-        if (nums.size()==1 && nums[i] != val) return 1;  
-        if (nums.size()==1 && nums[i] == val) return 0;  
-        while(i<=j)
-        {
-            if(nums[j]==val) j--;
-            else if(nums[i]==val)
-            {
-                nums[i] = nums[j];
+        while (i <= j) {
+            if (nums[i] == val) {
+                while (i <= j && nums[j] == val) {
+                    j--;
+                }
+                if (i <= j) {
+                    nums[i] = nums[j];
+                    j--;
+                    i++;
+                }
+            } 
+            else {
                 i++;
-                j--;
             }
-            else i++;
         }
         return i;
     }
