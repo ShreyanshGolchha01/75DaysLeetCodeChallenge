b35fba57a3e24db7763c9a78643ce8e266f92c03
Shreyansh Golchha <shreyansh.golchha2023@ssipmt.com>
2026-09-26 16:55:11 +0530

    Time: 0 ms (100%), Space: 20 MB (83.13%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int start = prices[0];
+        int ans = 0;
+        for(int i = 1;i<prices.size();i++)
+        {
+            if(start < prices[i]) ans+= prices[i] - start;
+            start =  prices[i];
+        }
+        return ans;
+    }
+};
